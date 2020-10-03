43c5ee780e99d7fe1a8bf29b5daad5f8fa63d98d
juan carlos <junkedev@gmail.com>
2020-10-02 20:21:48 -0400

    set work environment

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMaRjXpa6cilx17ahv-53eXJ5l9NfHKqI",
+  authDomain: "clone-83616.firebaseapp.com",
+  databaseURL: "https://clone-83616.firebaseio.com",
+  projectId: "clone-83616",
+  storageBucket: "clone-83616.appspot.com",
+  messagingSenderId: "748305419788",
+  appId: "1:748305419788:web:d5493ef793d8399e8a2829"
+};
